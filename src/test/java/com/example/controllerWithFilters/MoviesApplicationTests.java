5fb1adceabb7257d07ffaa4845076c0cf23a23cd
Behnam Ardalani <b.ardalani@gmail.com>
2023-09-06 17:57:30 -0400

    Initial project setup

---
+++ src/test/java/com/example/controllerWithFilters/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controllerWithFilters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
