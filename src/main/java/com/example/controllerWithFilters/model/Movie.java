7be7842c98f5d159b1578019a85bb1204c78334e
Behnam Ardalani <behnam.ardalani@workjam.com>
2023-09-09 20:46:59 -0400

    Fix the problem with the Model and DB

--- src/main/java/com/example/controllerWithFilters/model/Movie.java
+++ src/main/java/com/example/controllerWithFilters/model/Movie.java
@@ -3,30 +3,28 @@ package com.example.controllerWithFilters.model;
 import com.example.controllerWithFilters.model.enums.GenreEnum;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 import java.time.Instant;
 import java.util.UUID;
 
-@Table(name = "movie")
+@Entity
 @Data
+@Table(name = "movie")
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     @Id
     @Column(name = "id")
     private UUID id;
 
     @Column(name = "title")
-    @NonNull
     private String title;
 
     @Column(name = "description")
     private String description;
 
     @Enumerated(EnumType.STRING)
-    @NonNull
     @Column(name = "genre")
     private GenreEnum genre;
 
@@ -34,7 +32,6 @@ public class Movie {
     private Float rating;
 
     @Column(name = "release_date")
-    @NonNull
     @Builder.Default
     private Instant releaseDate = Instant.now();
 
--- src/main/java/com/example/controllerWithFilters/repository/MovieRepository.java
+++ src/main/java/com/example/controllerWithFilters/repository/MovieRepository.java
@@ -2,8 +2,10 @@ package com.example.controllerWithFilters.repository;
 
 import com.example.controllerWithFilters.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, UUID> {
 }
